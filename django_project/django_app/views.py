5ea0c55e7d245f0abd3abc20a3c9ef7e04b570f8
lefreitas.54@gmail.com <lefreitas.54@gmail.com>
2016-10-06 16:21:01 -0300

    primeiro commit

---
+++ django_project/django_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+	dicionario_contexto = {'msgnegrito': "Testando fonte em negrito..."}
+	return render(request, 'django/index.html', dicionario_contexto)
