75f588e27777a470f3920f4b7dee18e79da8f83e
leandrovox <lefreitas.54@gmail.com>
2016-10-09 12:28:01 -0300

    alterado models e views py

--- django_project/django_app/admin.py
+++ django_project/django_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django_app.models import Blog, Categoria
 
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+	exclude = ['data']
+	prepopulated_fields = {'slug': ('titulo', )}
+
+class CategoriaAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('titulo',)}
+
+admin.site.register(Blog)
+admin.site.register(Categoria)
